15492ad0b69147c5322e6fb49897c73b75f07090
Isaac Del Castillo <isaacdc03@hotmail.com>
2024-09-20 16:08:14 -0400

    Revamped Mobile compatability
    Mobile view is cleaner, and changed projects content

--- js/script.js
+++ js/script.js
@@ -1,22 +1,3 @@
-// Loading Screen
-const loadingScreen = document.querySelector(".loading-screen");
-const content = document.querySelector(".content");
-const listItems = document.querySelectorAll(".loading-screen ul li");
-
-function loadingAnimationComplete() {
-  content.classList.add("show");
-  loadingScreen.style.opacity = "0";
-  loadingScreen.style.visibility = "hidden";
-
-}
-
-listItems[listItems.length - 1].addEventListener(
-  "animationend",
-  loadingAnimationComplete
-);
-
-window.addEventListener("load", onLoad);
-
 //Footer Date
 function onLoad() {
   const copyrightEl = document.getElementById("copyrightText");
@@ -33,3 +14,4 @@ const underlineProjects = document.querySelectorAll(".projects h2");
 underlineProjects.forEach((h2) => {
   h2.style.textDecoration = "underline";
 });
+
